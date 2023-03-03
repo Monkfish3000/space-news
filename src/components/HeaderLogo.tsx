0a8370f02dcd8cf4a8ef4473b77ba5c0e40cea17
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-03-03 17:17:57 +0100

    initial setup complete, tailwind added, searchbar added with functionality

---
+++ src/components/HeaderLogo.tsx
@@ -0,0 +1,10 @@
+export const HeaderLogo = () => {
+  return (
+    <div className="self-center font-mono font-bold text-3xl text-white tracking-tighter">
+      <span>S</span>
+      <span className="hidden sm:inline-block">pace</span>
+      <span>N</span>
+      <span className="hidden sm:inline-block">ews</span>
+    </div>
+  );
+};
